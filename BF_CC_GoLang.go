fe2608fa54ccab8fee3991e0c80b7466c6a3fcab
John Gavigan <jsg5514@psu.edu>
2023-07-09 23:58:38 -0400

    Golang for first project

--- BF_CC_GoLang.go
+++ BF_CC_GoLang.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	cipherText := "Ugew gnwj zwjw Oslkgf"
+
+	for i := 0; i < 26; i++ {
+		plainText := ""
+		for _, char := range cipherText {
+			if char >= 'a' && char <= 'z' {
+				nChar := rune((int(char-'a')-i+26)%26 + 'a')
+				plainText += string(nChar)
+			} else if char >= 'A' && char <= 'Z' {
+				nChar := rune((int(char-'A')-i+26)%26 + 'A')
+				plainText += string(nChar)
+			} else {
+				plainText += string(char)
+			}
+		}
+		fmt.Printf("Iteration %d: %s\n", i, plainText)
+	}
 }
